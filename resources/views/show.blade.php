c86576fb55d7bf4cecac9aff4d89f24190ac63cf
Ahror Ahror <yuldashovahror4@gmail.com>
2021-09-29 16:04:46 +0500

    show.blade

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,16 +5,22 @@
 <div class="main-content">
     @foreach ($categories as $category)
     <div class="container">
-        <h2 class="title">Product</h2>
+        
         <div class="img-container">
             <div class="row justify-content-between">
-                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12"><img src="asset(public_path({{$category->image_1}}))" alt="9" class="img-fluid"></div>
-                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12"><img src="asset(public_path({{$category->image_1}}))" alt="9" class="img-fluid"></div>
-                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12"><img src="asset(public_path({{$category->image_1}}))" alt="9" class="img-fluid"></div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12"><img src="{{ $category->image_1 }}" alt="9" style="width: 250px; height: 250px; object-fit: cover;" class="img-fluid"></div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12"><img src="{{ $category->image_2 }}" alt="9" style="width: 250px; height: 250px; object-fit: cover;" class="img-fluid"></div>
+                    <div class="col-xxl-4 col-xl-4 col-lg-4 col-md-4 col-sm-12"><img src="{{ $category->image_3 }}" alt="9" style="width: 250px; height: 250px; object-fit: cover;" class="img-fluid"></div>
                 </div>
             </div>
             <div>
-                <p>{{ $category->summernote_uz }}</p>
+                <h2 class="title">UZ</h2>
+                <p>{!! $category->summernote_uz !!}</p>
+            </div>
+
+            <div>
+                <h2 class="title">RU</h2>
+                <p>{!! $category->summernote_ru !!}</p>
             </div>
         </div>
     </div>
